d94753e55a1e8f55d3a347e377b3b6b43bbc19fd
Steve Ziegler <stephen.v.ziegler@gmail.com>
2021-02-11 23:16:44 -0500

    updated tests

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -6,7 +6,7 @@ from analyze_git_csv import *
 class UnitTests(unittest.TestCase):
     def test_read_analytics__given_valid_csv_file__then_df_returned(self):
         # Arrange
-        input = "tests/data/short_sample.csv"
+        input = "tests/data/git_log_analysis.csv"
 
         # Act
         results = read_analytics(input)
@@ -14,16 +14,18 @@ class UnitTests(unittest.TestCase):
         print(results)
 
         # Assert
-        self.assertEqual(results.shape, (1, 14))
+        self.assertEqual(results.shape, (17, 14))
 
-    def test_calculate_file_complexity__given_input_dir_of_files__files_scores_returned(self):
+    def test_calculate_file_complexity__given_input_dir_of_files__files_scores_returned(
+        self,
+    ):
         # Arrange
-    
+
         # Act
-    
+
         # Assert
-    
-        
+        pass
+
 
 if __name__ == "__main__":
     unittest.main()
